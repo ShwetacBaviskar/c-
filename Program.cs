4c7f32ed54358ae06cb525aa5ec344d99b20e7a5
ShwetacBaviskar <158429764+ShwetacBaviskar@users.noreply.github.com>
2025-01-24 21:10:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace arr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = { { 1, 3, 5, 5 },{3,6,8,9} };
+            Console.WriteLine("Rank"+arr1.Rank);
+            ArrayList list = new ArrayList();
+            list.Add(2);
+            list.Add(5);
+            list.Add(6);
+            list.Add(8);
+            list.Add(9);
+            foreach(var i in list)
+            {
+                Console.WriteLine("" + i);
+            }
+
+        }
+    }
+}
